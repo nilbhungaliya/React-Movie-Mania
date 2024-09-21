10a2bf28883f046a5c4e285b04df59a3b2dc942b
nilbhungaliya <nilbhungaliya1204@gmail.com>
2024-09-21 12:28:26 +0530

    tvslice updated

--- src/Store/actions/TvAction.jsx
+++ src/Store/actions/TvAction.jsx
@@ -1,6 +1,6 @@
-export { removetv } from "../reducers/TvSlice";
+export { removetv } from "../reducers/tvSlice";
 import axios from "../../utils/axios";
-import { loadtv } from "../reducers/TvSlice";
+import { loadtv } from "../reducers/tvSlice";
 
 export const asyncloadtv = (id) => async (dispatch, getState) => {
     try {
